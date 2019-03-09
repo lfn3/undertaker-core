e303eec9a8237bfc39e070663bd9c9cef78b29bd
Liam Falconer <liam@lfn3.net>
2019-03-09 09:22:19 +0000

    Benchmark and dramatically simplify the moveIntoAnyRange function.
    This does make the distribution less fair - small ranges are now just as
    likely to get picked as larger ones. I'm hoping I'll be able to recover
    some of the fairness later with more sophisticated sources use the range
    information in their generation strategy.

---
+++ src/jmh/java/net/lfn3/undertaker/core/BytesBench.java
@@ -0,0 +1,51 @@
+package net.lfn3.undertaker.core;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+@State(Scope.Benchmark)
+public class BytesBench {
+    private static final int SIZE = Long.BYTES;
+    private static final ByteBuffer[] BUFFERS = new ByteBuffer[SIZE];
+    private static final Ranges[] RANGES = new Ranges[SIZE];
+
+    @Setup
+    public void setup() {
+        Random rand = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            byte[] wrapped = new byte[SIZE];
+            BUFFERS[i] = ByteBuffer.wrap(wrapped);
+            rand.nextBytes(wrapped);
+
+            byte[] range = new byte[SIZE * 4];
+            rand.nextBytes(range);
+
+            range[0] = -128;
+            range[SIZE] = -1;
+            range[SIZE * 2] = 0;
+            range[SIZE * 3] = 127;
+
+            RANGES[i] = Ranges.fromFlatArray(range, SIZE);
+        }
+    }
+
+    @Benchmark
+    public ByteBuffer[] moveIntoRangeBenchmark() {
+        for (int i = 0; i < SIZE; i++) {
+            Bytes.moveIntoAnyRange(BUFFERS[i], RANGES[i]);
+        }
+
+        return BUFFERS;
+    }
+
+    public static void main(String[] args) {
+        final BytesBench bench = new BytesBench();
+        bench.setup();
+        bench.moveIntoRangeBenchmark();
+    }
+}
--- src/main/java/net/lfn3/undertaker/core/Bytes.java
+++ src/main/java/net/lfn3/undertaker/core/Bytes.java
@@ -4,6 +4,8 @@ import net.lfn3.undertaker.core.Range.Bound;
 
 import java.nio.ByteBuffer;
 
+import static java.lang.Math.abs;
+
 public class Bytes {
     /**
      * This attempts to maintain the randomness from the source buffer.
@@ -15,84 +17,19 @@ public class Bytes {
     public static void moveIntoAnyRange(final ByteBuffer buf, final Ranges ranges) {
         Debug.userAssert(buf.capacity() == ranges.length, "Buffer should be the same length as the range you're trying to move it into");
 
-        //These are effectively final, only set once during the loop execution below.
-        int differentMsbIdx = -1;
-        Range selectedRange = null;
-
-        //Only a single range, we can just skip over the first two bits of the loop below.
-        if (ranges.numberOfRanges == 1) {
-            differentMsbIdx = 0;
-            selectedRange = ranges.get(0);
-        }
+        final Range selectedRange = ranges.get(abs(buf.get(0) % ranges.numberOfRanges));
 
         boolean considerUpperBound = true;
         boolean considerLowerBound = true;
 
-        for (int byteIdx = 0; byteIdx < ranges.length; byteIdx++) {
-            if (differentMsbIdx == -1) {
-                final byte firstRangeLowerBound = ranges.get(0, byteIdx, Bound.LOWER);
-                for (int rangeIdx = 1; rangeIdx < ranges.numberOfRanges; rangeIdx++) {
-                    final byte lowerBound = ranges.get(rangeIdx, byteIdx, Bound.LOWER);
-                    if (firstRangeLowerBound != lowerBound) {
-                        differentMsbIdx = byteIdx;
-                        break;
-                    }
-                }
-
-                if (differentMsbIdx == -1) {
-                    //All the ranges are the same up until this point, just set the byte to the range value
-                    buf.put(byteIdx, firstRangeLowerBound);
-                    continue;
-                }
-            }
-
-            Debug.devAssert(differentMsbIdx != -1, "MSB index should have been set by now.");
-
-            //At this point differentMsbIdx should contain the index of the point at which the ranges diverge.
-            //We pick a range based on the value at this idx.
-            //All the earlier bytes in the buffer should have already been moved into the range.
-            //TODO: most of the logic in here could use bytes if I go through and sort out the signing.
-            //TODO: could possible fuse this with the above loop as well.
-            final int valAtIdx = 0xff & buf.get(byteIdx);
-
-            if (selectedRange == null) {
-                final int[] msbSkip = new int[ranges.numberOfRanges];
-
-                {
-                    final int lowerBound = 0xff & ranges.get(0, byteIdx, Bound.LOWER);
-                    final int upperBound = 0xff & ranges.get(0, byteIdx, Bound.UPPER);
-
-                    msbSkip[0] = (upperBound - lowerBound) + 1; //Ranges are inclusive
-                }
-
-                for (int rangeIndex = 1; rangeIndex < ranges.numberOfRanges; rangeIndex++) {
-                    final int lowerBound = 0xff & ranges.get(rangeIndex, byteIdx, Bound.LOWER);
-                    final int upperBound = 0xff & ranges.get(rangeIndex, byteIdx, Bound.UPPER);
-
-                    msbSkip[rangeIndex] = msbSkip[rangeIndex - 1] + (upperBound - lowerBound);
-                }
-
-                int withinRange = valAtIdx % msbSkip[msbSkip.length - 1];
-
-                for (int i = 0; i < msbSkip.length; i++) {
-                    if (withinRange <= msbSkip[i]) {
-                        selectedRange = ranges.get(i);
-                    }
-                }
-            }
-
-            // Actually move it into the selected range (finally!)
-
-            Debug.devAssert(selectedRange != null, "Selected range should have been set by this point.");
-            //There's a DIY assertion right above this.
-            //noinspection ConstantConditions
+        for (int byteIdx = 0; byteIdx < selectedRange.length; byteIdx++) {
             final int lowerBound =  0xff & (considerLowerBound ? selectedRange.get( byteIdx, Bound.LOWER) : 0);
             final int upperBound =  0xff & (considerUpperBound ? selectedRange.get( byteIdx, Bound.UPPER) : -1);
             final int range = (upperBound - lowerBound) + 1;
 
             Debug.devAssert(Integer.signum(range) > 0, "Ranges must contain at least a single value");
 
-            final int valAtIdxInRange = (valAtIdx % range) + lowerBound;
+            final int valAtIdxInRange = ((0xff & buf.get(byteIdx)) % range) + lowerBound;
 
             Debug.devAssert(lowerBound <= valAtIdxInRange, "Should be gte lower bound");
             Debug.devAssert(valAtIdxInRange <= upperBound, "Should be lte upper bound");
@@ -107,10 +44,7 @@ public class Bytes {
                 considerLowerBound = false;
             }
         }
-        Debug.devAssert(selectedRange != null, "Should have got a selected range at this point");
-        //It is an assertion.
-        //noinspection ConstantConditions
+
         Debug.devAssert(selectedRange.isIn(buf.array()), "Value should have been moved into this range");
     }
-
 }
